d4ed6e1d9fb5bc6e9dfb1f4edc91fe9792a8dddf
Oscar Quintero <oscar24quintero@hotmail.com>
2021-10-09 14:50:28 -0500

    Instrucciones generales del programa

--- knn.py
+++ knn.py
@@ -3,3 +3,18 @@
 print("Actividad 5.6 - Implementación ")
 print("Algoritmo K Nearest Neighbor")
 print("\n")
+print("Cargar archivo desde el CSV")
+print("Cargar el data set")
+print("Pedir al usuario establecer el porcentaje de entrenamiento")
+print("Calcular número de instancias para cada conjunto")
+print("Separar en dos conjuntos")
+print("Generar conjunto de entrenamiento")
+print("Generar conjunto de prueba")
+
+print("Algoritmo KNN ....")
+print("Evaluar procentaje de aciertos o error cuadrático medio")
+print("Evaluar capacidad predicitiva")
+print("\n")
+
+
+
